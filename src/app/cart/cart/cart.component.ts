b65a2f780f3be7d89cf3d3c4d2d98ac9e6fb1754
Vineet Arora <37164735+VineetArora10@users.noreply.github.com>
2021-05-25 17:29:06 +0530

    Update cart.component.ts

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -13,8 +13,18 @@ export class CartComponent implements OnInit {
   public auser: any;
   public total = 0;
   public cart: any;
+  public mycart = [];
   public ucart: any;
   public usercart = [];
+  public curr: any;
+  public vcart : any;
+  public clicked = false;
+  public numcart: any;
+  public name: any;
+  public email: any;
+  public message: any;
+  public msg: any;
+  public city: any;
 
   constructor(
     private service: MyservicesService,
@@ -31,10 +41,13 @@ export class CartComponent implements OnInit {
       this.ucart = data;
       this.ucart.forEach(element => {
         if(element.id == obj.id){
+          this.vcart = element;
           this.usercart=element.cart;
           console.log(this.usercart);
+          this.numcart = this.usercart.length;
           this.usercart.forEach(element =>{
-            this.total += element.quantity*element.price;
+            this.total+=element.quantity*element.price;
+            this.msg += element.productname + ", "; 
           })
         }
     })
@@ -53,5 +66,62 @@ export class CartComponent implements OnInit {
   gotohome() {
     this.router.navigateByUrl('home');
   }
+  
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_i8havmt', 'template_0f3osze', e.target as HTMLFormElement, 'user_zcxt2aol1gKAdqlEdOXKS')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  checkout() {
+    console.log(this.vcart);
+    this.name = this.vcart.name;
+    this.email = this.vcart.email;
+    this.city = this.vcart.address;
+    this.message = "Your Order is Confirmed of " + this.msg + " and will be delivered to " + this.city + ". The total payable Amount for your order is ₹" + this.total + ". Thanks for Shopping we will wait for you!";
+    this.usercart = [];
+    this.vcart.cart=[];
+    this.total = 0;
+    this.service.updateusersdata(this.vcart.id, this.vcart).subscribe(data=>{
+    })
+  }
+  
+  add(index) {
+    this.usercart[index].quantity += 1;
+    this.vcart.cart = this.usercart;
+    this.service.updateusersdata(this.vcart.id, this.vcart).subscribe(data=>{
+      this.total=0;
+      this.usercart.forEach(element => {
+      this.total+=element.quantity*element.price;
+    });
+    })
+  }
+
+  remove(index) {
+    if(this.usercart[index].quantity == 1) {
+      this.usercart.splice(index,1);
+      this.numcart = this.usercart.length;
+      this.vcart.cart = this.usercart;
+      this.service.updateusersdata(this.vcart.id, this.vcart).subscribe(data=>{
+      this.total=0;
+      this.usercart.forEach(element => {
+      this.total += element.quantity*element.price;
+    })
+  })
+    } else {
+    this.usercart[index].quantity -= 1;
+    this.vcart.cart = this.usercart;
+    this.service.updateusersdata(this.vcart.id, this.vcart).subscribe(data=>{
+      this.total=0;
+      this.usercart.forEach(element => {
+      this.total += element.quantity*element.price;
+    })
+  })
+  }
+}
 
 }
